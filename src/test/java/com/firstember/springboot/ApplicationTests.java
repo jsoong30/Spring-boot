5252cefc6a9ac0faa9c76038122b5304e3a3bd50
Jackson J Soong <jacksonjsoong@gmail.com>
2025-08-08 21:59:56 -0400

    controller

---
+++ src/test/java/com/firstember/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstember.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
